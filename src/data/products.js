482954dcf2469e55da06af16b7a1a30f0562b7e0
scarlat-trainer <78.cristi@gmail.com>
2024-07-16 18:57:18 +0300

    add images in pub;lic, add cards example for products

---
+++ src/data/products.js
@@ -0,0 +1,18 @@
+
+
+export const products = [
+    {
+        name: "miere de tei",
+        price: 100,
+        weigth: "500g",
+        photo: "images/home-honey.jpg",
+        description: "bla bla miere de tei"
+    },
+    {
+        name: "miere de salcam",
+        price: 200,
+        weigth: "500g",
+        photo: "images/honey-salcam.jpg",
+        description: "bla bla miere de tei"
+    }
+]
